91387955aa29876e9b225d430ceb138248a9d681
apellanr <ryan.apellanes@gmail.com>
2017-04-23 01:07:16 -0700

    added to calculate shipping function. still need to implement calculation for arrival date

--- shipping.js
+++ shipping.js
@@ -16,13 +16,15 @@ function createNewShipment() {
 function ShipmentCalculation() {
     this.self = this;
     this.shippingTime = 5;
-    this.currentInput = [];
+    this.keyInput = [];
+    this.decimal = false;
     this.init = function() {
         this.textInput = $('#weightInput');
         this.radioButton = $('.radio');
         this.submit = $('#submitButton');
         this.applyEventHandlers();
     };
+
     // **** Event Handlers **** //
     this.applyEventHandlers = function() {
         // add a keydown handler to the text input that calls "validate_keypress"
@@ -33,12 +35,28 @@ function ShipmentCalculation() {
         this.submit.click(this.displayShipping);
     };
 
+    this.handleKeypress = function(value) {
+
+
+        this.validateKeypress();
+        this.checkDecimals();
+        this.truncateZeros();
+    };
+
     // **** Keypress Validation **** //
     this.validateKeypress = function(event) {
         if(event.keyCode === 13) { //enables users to press the enter to submit to display shipment calc
             this.displayShipping(); // calls display shipping function once enter pressed
         }
         return (event.which === 46 || event.which > 47 && event.which < 58);
+    };
+
+    this.checkDecimals = function() {
+
+    };
+
+    this.truncateZeros = function() {
+
     };
 
     // **** Change Shipping Type **** //
@@ -48,11 +66,37 @@ function ShipmentCalculation() {
     };
 
     // **** Calculate Shipping **** //
-    this.calculateShipping = function(weight, shipping_time) {
+    this.calculateShipping = function(weight, time) {
+        this.ounces = weight * 16;
+        this.cost = 0;
+
+        if(this.ounces < 20) {
+            this.cost = this.ounces * .02;
+        } else if (this.ounces > 32) {
+            this.cost = this.ounces * .20;
+        } else {
+            this.cost = this.ounces * .10;
+        }
+
+        switch(time) {
+            case 5:
+                this.cost *= 1;
+                break;
+            case 3:
+                this.cost *= 1.5;
+                break;
+            case 2:
+                this.cost *= 2;
+                break;
+        }
 
 
-        // object required to return
-
+        this.shipmentObj = { // return object with properties and values:
+            "arrival_date": this.deliveryTime, // 1) arrival date (string)
+            "weight": this.ounces, // 2) weight (number) - the weight (in ounces) of the package
+            "cost": this.costOfPackage // 3) cost (number) - the cost (in dollars) of shipping
+        };
+        return this.shipmentObj;
     };
 
     // **** Display Shipping **** //
